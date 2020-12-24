e3d24ab27c32e02fd7f53601e958b6c0c1fac6b9
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-12-24 11:54:28 +0900

    カテゴリーモデルの作成

---
+++ app/models/category.rb
@@ -0,0 +1,17 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '食費' },
+    { id: 3, name: '日用雑貨' },
+    { id: 4, name: '交通' },
+    { id: 5, name: '通信' },
+    { id: 6, name: '水道・光熱' },
+    { id: 7, name: '住まい' },
+    { id: 8, name: 'その他雑費' }
+  ]
+   
+  # アソシエーション
+   include ActiveHash::Associations
+   has_many :purchases
+ 
+end
