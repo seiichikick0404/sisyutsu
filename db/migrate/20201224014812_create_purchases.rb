e1e5ccf1bfc3cd0ca45c0444f1e6dcd8e3bd6af6
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-12-24 11:25:27 +0900

    purchaseテーブルの作成

--- db/migrate/20201224014812_create_purchases.rb
+++ db/migrate/20201224014812_create_purchases.rb
@@ -1,7 +1,9 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.string    :text, null: false
+      t.integer   :category_id, null: false
+      t.integer   :price, null: false
       t.timestamps
     end
   end
