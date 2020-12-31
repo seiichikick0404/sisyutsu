f90f1ae017200ee288f41ab324f9a4664ae004c0
seiichi0404 <yamayamaseisei1834@gmail.com>
2020-12-31 15:40:06 +0900

    エラーハンドリング

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,18 @@
 class PurchasesController < ApplicationController
   def index
-  end
 
-  def new
-    Purchase.new
   end
 
   def create
     @purchase = Purchase.new(purchase_params)
-    @purchase.save
+    if @purchase.save
+      redirect_to root_path
+    end
   end
 
 
   private
   def purchase_params
-    params.require(:purchase).permit(:text, :price, :category_id)
+    params.require(:purchase).permit(:text, :category_id,:price)
   end
 end
