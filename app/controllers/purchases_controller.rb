b44fd3f0f3805c20b2a8646f12d356fe16cc154c
seiichi0404 <yamayamaseisei1834@gmail.com>
2021-01-03 13:27:48 +0900

    1週間の出費を表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,12 +3,18 @@ class PurchasesController < ApplicationController
     query = "SELECT *
     FROM purchases
     WHERE created_at >= CURRENT_DATE"
-    @result_today = Purchase.find_by_sql(query)
-    
-    
-    @result_today.each do |result| 
-      result.price
-    end 
+    @result_today = Purchase.find_by_sql(query)   #今日の出費のデータ
+
+    @purchases = Purchase.all 
+    @purchase_week = 0
+    this_week = Date.today.all_week # all_weekをDate.todayに適用すると、今週の年月日データを取得できる。
+      @purchases.each do |purchase| 
+        if (this_week.include?(Date.parse(purchase[:created_at].to_s)))
+            @purchase_week += purchase[:price] 
+        end
+      end
+      
+
   end
 
   def create
