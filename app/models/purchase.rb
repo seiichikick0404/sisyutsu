df6ccd58ca02b065a4a1bbf4f1af000c7977e68b
seiichi0404 <yamayamaseisei1834@gmail.com>
2021-01-06 07:51:27 +0900

    フォームが空欄の時は通さない

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,11 @@
 class Purchase < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+
+  # バリデーション
+  with_options presence: true do
+    validates :text
+    validates :price
+    validates :category_id
+  end
 end
